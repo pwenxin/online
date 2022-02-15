9a1945635a5445c329e46b393f8d500047fec292
page <1243202465@qq.com>
2022-02-15 23:05:20 +0800

    init

--- app/src/main/java/com/sharews/online/MainActivity.java
+++ app/src/main/java/com/sharews/online/MainActivity.java
@@ -1,11 +1,18 @@
 package com.sharews.online;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.TypedValue;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             else {
                 intent.setAction(MainService.ACTION_START);
             }
-            mButtonToggle.setEnabled(false);
+//            mButtonToggle.setEnabled(false);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 startForegroundService(intent);
@@ -44,7 +51,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mButtonReverseVNC.setOnClickListener(view -> {
+            final EditText inputText = new EditText(this);
+            inputText.setInputType(InputType.TYPE_CLASS_TEXT);
+            inputText.setHint(getString(R.string.main_activity_reverse_vnc_hint));
+            inputText.requestFocus();
+            inputText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
+            LinearLayout inputLayout = new LinearLayout(this);
+            inputLayout.setPadding(
+                    (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, Resources.getSystem().getDisplayMetrics()),
+                    (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, Resources.getSystem().getDisplayMetrics()),
+                    (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, Resources.getSystem().getDisplayMetrics()),
+                    0
+            );
+            inputLayout.addView(inputText);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setView(inputLayout)
+                    .create();
+            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            dialog.show();
         });
     }
 }
--- app/src/main/java/com/sharews/online/MainService.java
+++ app/src/main/java/com/sharews/online/MainService.java
@@ -1,7 +1,10 @@
 package com.sharews.online;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -10,12 +13,66 @@ public class MainService extends Service {
     final static String ACTION_START = "start";
     final static String ACTION_STOP = "stop";
 
+    final static String ACTION_HANDLE_MEDIA_PROJECTION_RESULT = "action_handle_media_projection_result";
+    final static String EXTRA_MEDIA_PROJECTION_RESULT_DATA = "result_data_media_projection";
+    final static String EXTRA_MEDIA_PROJECTION_RESULT_CODE = "result_code_media_projection";
+    final static String ACTION_HANDLE_INPUT_RESULT = "action_handle_a11y_result";
+    final static String EXTRA_INPUT_RESULT = "result_a11y";
+    final static String ACTION_HANDLE_WRITE_STORAGE_RESULT = "action_handle_write_storage_result";
+    final static String EXTRA_WRITE_STORAGE_RESULT = "result_write_storage";
+
+    private MediaProjectionManager mMediaProjectionManager;
+    private MediaProjection mMediaProjection;
+    private int mResultCode;
+    private Intent mResultData;
+
+    public enum StatusEvent {
+        STARTED,
+        STOPPED,
+    }
+
     public MainService() {
     }
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "onCreate: Mainrservice");
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Log.i(TAG, "onCreate: MainService");
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if (ACTION_HANDLE_MEDIA_PROJECTION_RESULT.equals(intent.getAction())) {
+            Log.d(TAG, "onStartCommand: handle media projection result");
+            // Step 4 (optional): coming back from capturing permission check, now starting capturing machinery
+            mResultCode = intent.getIntExtra(EXTRA_MEDIA_PROJECTION_RESULT_CODE, 0);
+            mResultData = intent.getParcelableExtra(EXTRA_MEDIA_PROJECTION_RESULT_DATA);
+            startScreenCapture();
+        }
+
+        if (ACTION_HANDLE_WRITE_STORAGE_RESULT.equals(intent.getAction())) {
+            Log.d(TAG, "onStartCommand: handle write storage result");
+            // Step 3: coming back from write storage permission check, start capturing
+            // or ask for ask for capturing permission first (then going in step 4)
+            if (mResultCode != 0 && mResultData != null) {
+                startScreenCapture();
+            } else {
+                Log.i(TAG, "Requesting confirmation");
+                // This initiates a prompt dialog for the user to confirm screen projection.
+                Intent mediaProjectionRequestIntent = new Intent(this, MediaProjectionRequestActivity.class);
+                mediaProjectionRequestIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(mediaProjectionRequestIntent);
+            }
+        }
+
+        Log.i(TAG, "onStartCommand: MainService");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startScreenCapture() {
+
     }
 
     @Override
